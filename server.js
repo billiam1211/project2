bb56244fd1d8d6a4c3331689021dcf29ced33117
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-04-24 16:04:24 -0500

    server and files created

---
+++ server.js
@@ -0,0 +1,21 @@
+const express        = require('express');
+const app            = express();
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const session        = require('express-session');
+require('./db/db')
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('listening... on port: ', 3000);
+});
